1afc9fab8df2a9098defa6380b9cbafc2613e412
glrh11 <glrh11@163.com>
2017-12-15 21:27:42 +0800

    增加BST其他操作

--- chapter_03_查找/3.bst.go
+++ chapter_03_查找/3.bst.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"errors"
+)
+
 // 二叉树实现的 ST
 
 /*
@@ -121,9 +125,62 @@ func (this *BST) Put(key *Comparable, value interface{}) {
 	}
 }
 
-// Delete 删除
-func (this *BST) Delete(key *Comparable) {
+// 是否成功删除，是否需要替换子结点，替换子结点的值 FIXME 参考P261 的简单实现
+func (this *BST) delete(node *BSTNode, key *Comparable) (ifDelete bool, ifReplace bool, replaceNode *BSTNode) {
+	if nil != node {
+		reComp := node.key.CompareTo(*key)
+		if reComp == 0 {   // 执行删除操作
+			// 判断有无后继结点。2种情况，1）只有一个子结点 2）有两个子结点
+			if node.leftNode == nil && node.rightNode == nil {
+				replaceNode = nil
+			} else if node.leftNode == nil {
+				replaceNode = node.rightNode
+			} else if node.rightNode == nil {
+				replaceNode = node.leftNode
+			} else { // 两个子结点. 将右边结点的最小值，替换为这个结点
+				rightMinNode := this.minNode(node.rightNode) // 返回key吗？
+				this.deleteMin(node.rightNode) // 孤立这个结点. 这里不会
 
+				rightMinNode.leftNode = node.leftNode
+				rightMinNode.rightNode = node.rightNode
+
+				rightMinNode.size = rightMinNode.leftNode.Size() + rightMinNode.rightNode.Size()
+
+				replaceNode = rightMinNode
+			}
+			ifReplace = true
+			ifDelete = true
+		} else if reComp < 0 { // 去右边. 更换右边的结点
+			subIfDelete, subIfReplace, subReplaceNode := this.delete(node.rightNode, key)
+			if subIfReplace {
+				node.rightNode = subReplaceNode
+				replaceNode = nil
+				ifReplace = false
+			}
+			if subIfDelete {
+				node.size--
+				ifDelete = true
+			}
+		} else {
+			subIfDelete, subIfReplace, subReplaceNode := this.delete(node.leftNode, key)
+			if subIfReplace {
+				node.leftNode = subReplaceNode
+				replaceNode = nil
+				ifReplace = false
+			}
+			if subIfDelete {
+				node.size--
+				ifDelete = true
+			}
+		}
+	}
+	return
+}
+
+// Delete 删除
+// 参见书本 page 260(总页码) T.Hibbard 方法
+func (this *BST) Delete(key *Comparable) {
+	this.delete(this.root, key)
 }
 
 func (this *BST) contains(node *BSTNode, key *Comparable) (ifContains bool) {
@@ -173,6 +230,270 @@ func (this *BST) Keys() (keys []*Comparable) {
 	return this.keys(this.root)
 }
 
+func (this *BST) minNode(node *BSTNode) (minN *BSTNode) {
+	if nil == node { // root 才会走到这里
+		minN = nil
+	} else if (node.leftNode == nil) {
+		//key = &Comparable{}
+		//*key = *node.key // 复制一份 FIXME 这里不执行复制操作，因为删除操作里边需要用到。
+		minN = node
+	} else {
+		minN = this.minNode(node.leftNode)
+	}
+	return
+}
+
+func (this *BST) min(node *BSTNode) (key *Comparable) {
+	minNode := this.minNode(node)
+	if nil != minNode {
+		key = minNode.key  // FIXME : 为了delete需要，选择不复制key
+	}
+	return
+}
+
+// Min 返回最小键，顺着左树查找
+func (this *BST) Min() (key *Comparable, err error) {
+	key = this.min(this.root)
+	if nil == key {
+		err = errors.New("BST is empty")
+	}
+	return
+}
+
+func (this *BST) max(node *BSTNode) (key *Comparable) {
+	if nil == node { // root 才会走到这里
+		key = nil
+	} else if (node.rightNode == nil) {
+		key = &Comparable{}
+		*key = *node.key // 复制一份
+	} else {
+		key = this.max(node.rightNode)
+	}
+	return
+}
+
+// Max 返回最小键，顺着左树查找
+func (this *BST) Max() (key *Comparable, err error) {
+	key = this.max(this.root)
+	if nil == key {
+		err = errors.New("BST is empty")
+	}
+	return
+}
+
+// 如果没找到，返回nil
+func (this *BST) floor(node *BSTNode, key *Comparable) (reKey *Comparable) {
+	if node == nil {
+		reKey = nil
+	} else {
+		// 跟本结点比较大小
+		reComp := node.key.CompareTo(*key)
+		if reComp == 0 {         // 返回这个值
+			reKey = node.key
+		} else if (reComp < 0) { // 试探右树上查找
+			reKey = this.floor(node.rightNode, key) // 判断返回值是否为nil
+			if reKey == nil {
+				reKey = node.key
+			}
+		} else {                 // 在左树上查找
+			reKey = this.floor(node.leftNode, key)
+		}
+	}
+	if nil != reKey {  // 复制一份
+		*reKey = *reKey
+	}
+	return
+}
+
+// Floor 小于等于 key 的最大键; 如果没找到，返回 err
+func (this *BST) Floor(key *Comparable) (reKey *Comparable, err error) {
+	reKey = this.floor(this.root, key)
+	if reKey == nil {
+		err = errors.New("No return key")
+	}
+	return
+}
+
+// ceiling 没找到返回 nil
+func (this *BST) ceiling(node *BSTNode, key *Comparable) (reKey *Comparable) {
+	if node == nil {
+		reKey = nil
+	} else {
+		// 跟本结点比较大小
+		reComp := node.key.CompareTo(*key)
+		if reComp == 0 {   // 返回这个值
+			reKey = node.key
+		} else if (reComp < 0) { // 试探右树上查找
+			reKey = this.ceiling(node.rightNode, key) // 判断返回值 nil
+		} else {                 // 在左树上查找
+			reKey = this.ceiling(node.leftNode, key)
+			if nil == reKey {     // 判断返回值是否为 nil
+				reKey = node.key
+			}
+		}
+	}
+	if nil != reKey {  // 复制一份
+		*reKey = *reKey
+	}
+	return
+}
+
+// Ceiling 大于等于key的最小值; 如果没找到，返回err
+func (this *BST) Ceiling(key *Comparable) (reKey *Comparable, err error) {
+	reKey = this.ceiling(this.root, key)
+	if reKey == nil {
+		err = errors.New("No return key")
+	}
+	return
+}
+
+// 应该不会返回nil
+func (this *BST) selectKey(node *BSTNode, index int) (key *Comparable) {
+	// 不用处理 结点为nil的情况
+	// 从左结点的size开始比较
+	leftSize := node.leftNode.Size()
+	if (leftSize < index) { // 右边树上找
+		key = this.selectKey(node.rightNode, index - leftSize - 1)
+	} else if (leftSize == index) { // 返回该结点的key
+		key = node.key
+	} else {                // 在左边树上找
+		key = this.selectKey(node.leftNode, index)
+	}
+	*key = *key // 复制一份
+	return
+}
+
+// SelectKey
+func (this *BST) SelectKey(index int) (key *Comparable, err error) {
+	// 检查index范围
+	if (index < 0 || index >= this.Size()) {
+		err = errors.New("Index out of BST!")
+	} else {
+		key = this.selectKey(this.root, index)
+	}
+	return
+}
+
+// 需要判断 nil
+func (this *BST) rank(node *BSTNode, key *Comparable) (rank int) {
+	if nil != node {
+		reComp := node.key.CompareTo(*key)
+		if (reComp == 0) { // 返回在这棵树上的位置 1 代表该结点
+			rank = node.leftNode.Size()
+		} else if (reComp < 0) { // 去右边树
+			rank = node.leftNode.Size() + 1 + this.rank(node.rightNode, key)
+		} else { // 在左边树上找
+			rank = this.rank(node.leftNode, key)
+		}
+	} else {
+		rank = 0
+	}
+	return
+}
+
+// Rank
+func (this *BST) Rank(key *Comparable) (rank int) {
+	return this.rank(this.root, key)
+}
+
+func (this *BST) deleteMin(node *BSTNode) (ifDelete bool, appNode *BSTNode, ifReplace bool) {
+	if node == nil {
+		ifDelete = false
+	} else {
+		subIfDelete, subAppNode, subIfReplace := this.deleteMin(node.leftNode)
+		if ! subIfDelete { // 如果没有删除，那么删除该结点
+			// 检查是否有右结点
+			ifReplace = true
+			ifDelete = true
+			appNode = node.rightNode
+		} else { // size--
+			node.size--
+			if subIfReplace {
+				node.leftNode = subAppNode
+				appNode = nil
+				ifReplace = false
+			}
+			ifDelete = true
+		}
+	}
+	return
+}
+
+// DeleteMin 删除最小值
+func (this *BST) DeleteMin() (err error) {
+	if ifDelete, _, _ := this.deleteMin(this.root); ! ifDelete {
+		err = errors.New("BST is Empty")
+	}
+	return
+}
+
+func (this *BST) deleteMax(node *BSTNode) (ifDelete bool, appNode *BSTNode, ifReplace bool) {
+	if node == nil {
+		ifDelete = false
+	} else {
+		subIfDelete, subAppNode, subIfReplace := this.deleteMax(node.rightNode)
+		if ! subIfDelete { // 如果没有删除，那么检查该结点情况
+			// 检查是否有右结点
+			ifReplace = true
+			ifDelete = true
+			appNode = node.leftNode
+		} else {       // size--
+			node.size--
+			if subIfReplace {
+				node.rightNode = subAppNode
+				appNode = nil
+				ifReplace = false
+			}
+			ifDelete = true
+		}
+	}
+	return
+}
+
+// DeleteMin 删除最小值 FIXME 实现有误
+func (this *BST) DeleteMax() (err error) {
+	if ifDelete, _, _ := this.deleteMax(this.root); ! ifDelete {
+		err = errors.New("BST is Empty")
+	}
+	return
+}
+
+// 两个key之间的键的数量 [lo, hi] 两端都包含
+func (this *BST) SizeBetween(lo *Comparable, hi *Comparable) (size int) {
+	size = 0
+
+	if lo.CompareTo(*hi) < 0 {
+		loRank := this.Rank(lo)
+		hiRank := this.Rank(hi) // 如果 hi 在 st内，那么 hi - lo + 1
+
+		if this.Contains(hi) {
+			size = hiRank - loRank + 1
+		} else {
+			size = hiRank - loRank
+		}
+	}
+
+	return
+}
+
+// 检查每个树上的，然后返回。不过效率太低了。FIXME 先不搞了，费头发。白天再弄。
+// ( ∞, lo)  1
+// [lo, hi]  2
+// (hi, ∞ )  3
+func (this *BST) keysBetween(node *BSTNode, keys []*Comparable, lo *Comparable, hi *Comparable) (status int) {
+
+	// walk tree 中序遍历
+	// 设置一个 status , 决定遍历区间
+
+	return
+}
+
+// 返回 [lo, hi] 之间的键的列表
+func (this *BST) KeysBetween(lo *Comparable, hi *Comparable) (keys []*Comparable) {
+	keys = []*Comparable{}
+	this.keysBetween(this.root, keys, lo, hi)
+	return
+}
 
 func readAndCountByBST(filename string, lengthThreshold int) (totalWordCount int, differendWordCount int) {
 	return ReadAndCount(
--- chapter_03_查找/3.bst_test.go
+++ chapter_03_查找/3.bst_test.go
@@ -21,7 +21,7 @@ func TestBST(t *testing.T) {
 	// 简单测试
 	Convey("测试基本内容", t, func() {
 		// 首先往里边push数据，看取出来的对不对。
-		var st SymbolTableInterface = NewBST()
+		var st SortedSymbolTableInterface = NewBST()
 		stMap := map[string]int{
 			"S": 0,
 			"Y": 1,
@@ -50,127 +50,139 @@ func TestBST(t *testing.T) {
 		for key, value := range stMap {
 			So(st.Get(NewComparable(key)), ShouldEqual, value)
 		}
-		//// 检测删除
-		//st.Delete(NewComparable("Y"))
-		//So(st.Get(NewComparable("Y")), ShouldEqual, nil)
+
+		// 检测删除
+		st.Delete(NewComparable("Y"))
+		So(st.Get(NewComparable("Y")), ShouldEqual, nil)
+
+		for _, value := range st.Keys() {
+			fmt.Println(value.Value())
+		}
+		fmt.Println(len(st.Keys()), st.Size())
+
 		// 检测size
-		So(st.Size(), ShouldEqual, 11)
+		So(st.Size(), ShouldEqual, 10)
 		// 检测keys
 
 		keyValueList := []string{}
 		for _, key := range st.Keys() {
 			keyValueList = append(keyValueList, key.Value().(string))
 		}
+		//
+		//for index, _ := range stMap {
+		//	if ("Y" == index) { continue }
+		//}
 
-		for index, _ := range stMap {
-			if ("Y" == index) { continue }
+		// 测试 Min
+		minKey, err := st.Min()
+		So(err, ShouldEqual, nil)
+		So(minKey.Value(), ShouldEqual, NewComparable("A").Value())
+
+		// 测试空ST MIN
+		var stEmpty SortedSymbolTableInterface = NewBinarySearchST()
+		minKey, err = stEmpty.Min()
+		So(err, ShouldNotEqual, nil)
+
+		// 测试 Max
+		maxKey, err := st.Max()
+		So(err, ShouldEqual, nil)
+		So(maxKey.Value(), ShouldEqual, NewComparable("X").Value())
+
+		// 测试空ST Max
+		maxKey, err = stEmpty.Min()
+		So(err, ShouldNotEqual, nil)
+
+		// 测试 Contains 在keys里边的，都应该为true
+		for _, key := range st.Keys() {
+			contains := st.Contains(key)
+			So(contains, ShouldEqual, true)
 		}
 
-		//// 测试 Min
-		//minKey, err := st.Min()
-		//So(err, ShouldEqual, nil)
-		//So(minKey.Value(), ShouldEqual, NewComparable("A").Value())
-		//
-		//// 测试空ST MIN
-		//var stEmpty SortedSymbolTableInterface = NewBinarySearchST()
-		//minKey, err = stEmpty.Min()
-		//So(err, ShouldNotEqual, nil)
-		//
-		//// 测试 Max
-		//maxKey, err := st.Max()
-		//So(err, ShouldEqual, nil)
-		//So(maxKey.Value(), ShouldEqual, NewComparable("X").Value())
-		//
-		//// 测试空ST Max
-		//maxKey, err = stEmpty.Min()
-		//So(err, ShouldNotEqual, nil)
-		//
-		//// 测试 Contains 在keys里边的，都应该为true
-		//for _, key := range st.Keys() {
-		//	contains := st.Contains(key)
-		//	So(contains, ShouldEqual, true)
-		//}
-		//
-		//contains := st.Contains(NewComparable("Y"))
-		//So(contains, ShouldEqual, false)
-		//
-		//// 测试 Floor 1) 用小于最小的测试 2) 用存在的测试 3) 用大于最大的测试
-		//floorKey, err := st.Floor(NewComparable("1")) // 应该返回 err
-		//So(err, ShouldNotEqual, nil)
-		//
-		//floorKey, err = st.Floor(NewComparable("A"))
-		//So(err, ShouldEqual, nil)
-		//So(floorKey.Value(), ShouldEqual, "A")
-		//
-		//floorKey, err = st.Floor(NewComparable("Y"))
-		//So(err, ShouldEqual, nil)
-		//So(floorKey.Value(), ShouldEqual, "X")
-		//
-		//// 测试 ceiling 1) 小于最小的测试 2) 存在的测试 3) 大于最大的测试
-		//ceilingKey, err := st.Ceiling(NewComparable("1")) // 应该返回 err
-		//So(err, ShouldEqual, nil)
-		//So(ceilingKey.Value(), ShouldEqual, "A")
-		//
-		//ceilingKey, err = st.Ceiling(NewComparable("A"))
-		//So(err, ShouldEqual, nil)
-		//So(ceilingKey.Value(), ShouldEqual, "A")
-		//
-		//ceilingKey, err = st.Ceiling(NewComparable("Y"))
-		//So(err, ShouldNotEqual, nil)
-		//
-		//// 测试 SelectKey 1) 边界的两个值 2) 超出边界的一个值 3) 中间的某个值
-		//selectedKey, err := st.SelectKey(0)
-		//So(selectedKey.Value(), ShouldEqual, st.Keys()[0].Value())
-		//So(err, ShouldEqual, nil)
-		//
-		//selectedKey, err = st.SelectKey(st.Size()-1)
-		//So(selectedKey.Value(), ShouldEqual, st.Keys()[st.Size()-1].Value())
-		//So(err, ShouldEqual, nil)
-		//
-		//selectedKey, err = st.SelectKey(st.Size())
-		//So(err, ShouldNotEqual, nil)
-		//
-		//selectedKey, err = st.SelectKey(2)
-		//So(selectedKey.Value(), ShouldEqual, "E")
-		//So(err, ShouldEqual, nil)
-		//
-		//// 测试 DeleteMin 1) 测试存在的 2) 测试空的
-		//// Now ABELMOPSTX 将会删除 A
-		//err = st.DeleteMin()
-		//So(err, ShouldEqual, nil)
-		//minKey, err = st.Min()
-		//So(minKey.Value(), ShouldEqual, "B")
-		//
-		//err = stEmpty.DeleteMin()
-		//So(err, ShouldNotEqual, nil)
-		//
-		//// 测试 DeleteMax 1) 测试存在的 2) 测试空的
-		//// Now BELMOPSTX 将会删除 X
-		//err = st.DeleteMax()
-		//So(err, ShouldEqual, nil)
-		//maxKey, err = st.Max()
-		//So(maxKey.Value(), ShouldEqual, "T")
-		//
-		//err = stEmpty.DeleteMax()
-		//So(err, ShouldNotEqual, nil)
-		//
-		//// SizeBetween 1) 测试两个key都存在于st中的 2) 测试一个key在st中的 3) 两个都不在 4) 测试 hi < lo 的
-		//// 现在key列表如下 BELMOPST
-		//size := st.SizeBetween(NewComparable("B"), NewComparable("M")) // ==4
-		//So(size, ShouldEqual, 4)
-		//
-		//size = st.SizeBetween(NewComparable("A"), NewComparable("M")) // ==4
-		//So(size, ShouldEqual, 4)
-		//
-		//size = st.SizeBetween(NewComparable("B"), NewComparable("N")) // ==4
-		//So(size, ShouldEqual, 4)
-		//
-		//size = st.SizeBetween(NewComparable("A"), NewComparable("Y")) // ==8
-		//So(size, ShouldEqual, 8)
-		//
-		//size = st.SizeBetween(NewComparable("T"), NewComparable("A")) // ==0
-		//So(size, ShouldEqual, 0)
-		//
+		contains := st.Contains(NewComparable("Y"))
+		So(contains, ShouldEqual, false)
+
+		// 测试 Floor 1) 用小于最小的测试 2) 用存在的测试 3) 用大于最大的测试
+		floorKey, err := st.Floor(NewComparable("1")) // 应该返回 err
+		So(err, ShouldNotEqual, nil)
+
+		floorKey, err = st.Floor(NewComparable("A"))
+		So(err, ShouldEqual, nil)
+		So(floorKey.Value(), ShouldEqual, "A")
+
+		floorKey, err = st.Floor(NewComparable("Y"))
+		So(err, ShouldEqual, nil)
+		So(floorKey.Value(), ShouldEqual, "X")
+
+		// 测试 ceiling 1) 小于最小的测试 2) 存在的测试 3) 大于最大的测试
+		ceilingKey, err := st.Ceiling(NewComparable("1")) // 应该返回 err
+		So(err, ShouldEqual, nil)
+		So(ceilingKey.Value(), ShouldEqual, "A")
+
+		ceilingKey, err = st.Ceiling(NewComparable("A"))
+		So(err, ShouldEqual, nil)
+		So(ceilingKey.Value(), ShouldEqual, "A")
+
+		ceilingKey, err = st.Ceiling(NewComparable("Y"))
+		So(err, ShouldNotEqual, nil)
+
+		// 测试 SelectKey 1) 边界的两个值 2) 超出边界的一个值 3) 中间的某个值 FIXME 这里有bug
+		for _, value := range st.Keys() {
+			fmt.Println(value.Value())
+		}
+
+		selectedKey, err := st.SelectKey(0)
+		So(err, ShouldEqual, nil)
+		fmt.Println("selectedKey is : ", selectedKey)
+		So(selectedKey.Value(), ShouldEqual, st.Keys()[0].Value())
+
+		selectedKey, err = st.SelectKey(st.Size()-1)
+		So(selectedKey.Value(), ShouldEqual, st.Keys()[st.Size()-1].Value())
+		So(err, ShouldEqual, nil)
+
+		selectedKey, err = st.SelectKey(st.Size())
+		So(err, ShouldNotEqual, nil)
+
+		selectedKey, err = st.SelectKey(2)
+		So(selectedKey.Value(), ShouldEqual, "E")
+		So(err, ShouldEqual, nil)
+
+		// 测试 DeleteMin 1) 测试存在的 2) 测试空的
+		// Now ABELMOPSTX 将会删除 A
+		err = st.DeleteMin()
+		So(err, ShouldEqual, nil)
+		minKey, err = st.Min()
+		So(minKey.Value(), ShouldEqual, "B")
+
+		err = stEmpty.DeleteMin()
+		So(err, ShouldNotEqual, nil)
+
+		// 测试 DeleteMax 1) 测试存在的 2) 测试空的
+		// Now BELMOPSTX 将会删除 X
+		err = st.DeleteMax()
+		So(err, ShouldEqual, nil)
+		maxKey, err = st.Max()
+		So(maxKey.Value(), ShouldEqual, "T")
+
+		err = stEmpty.DeleteMax()
+		So(err, ShouldNotEqual, nil)
+
+		// SizeBetween 1) 测试两个key都存在于st中的 2) 测试一个key在st中的 3) 两个都不在 4) 测试 hi < lo 的
+		// 现在key列表如下 BELMOPST
+		size := st.SizeBetween(NewComparable("B"), NewComparable("M")) // ==4
+		So(size, ShouldEqual, 4)
+
+		size = st.SizeBetween(NewComparable("A"), NewComparable("M")) // ==4
+		So(size, ShouldEqual, 4)
+
+		size = st.SizeBetween(NewComparable("B"), NewComparable("N")) // ==4
+		So(size, ShouldEqual, 4)
+
+		size = st.SizeBetween(NewComparable("A"), NewComparable("Y")) // ==8
+		So(size, ShouldEqual, 8)
+
+		size = st.SizeBetween(NewComparable("T"), NewComparable("A")) // ==0
+		So(size, ShouldEqual, 0)
+
 		//// KeysBetween 1) 测试两个key都存在于st中的 2) 测试一个key在st中的 3) 两个都不在 4) 测试 hi < lo 的
 		//keys := st.KeysBetween(NewComparable("B"), NewComparable("M")) // BELM
 		//So(len(keys), ShouldEqual, 4)
@@ -197,8 +209,8 @@ func TestBST(t *testing.T) {
 			start := time.Now()
 			// fixme
 			// 读取 tale 116 ms 比二分查找快了十倍
-			// 读取 leipzig1M   31651,9931,4420 ms
-			t, d := readAndCountByBST("exam/leipzig1M.txt", i)
+			// 读取 leipzig1M   31651,9931,4420 ms leipzig1M
+			t, d := readAndCountByBST("exam/tale.txt", i)
 			end := time.Now()
 			spendMilliSecond := end.Sub(start).Nanoseconds() / 1000000 // 毫秒
 			fmt.Printf("Total words [%d], Different words [%d], Spend MilliSeconds [%d]!\n", t, d, spendMilliSecond)
